173ed23f5d1f334659a334d58e2fd32eeb0aae79
luat <thaivanluat98@gmail.com>
2020-11-20 23:13:51 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "To do 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To do 2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "To do 3",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+
+    addTodo(newTodo) {
+      let id = this.todos.length + 1;
+      newTodo.id = id;
+      this.todos = [...this.todos, newTodo];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing:border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+      <form @submit="addTodo">
+          <input type="text" name="title"  v-model="title" placeholder="Add Todo ...">
+          <input type="submit" value="Submit" class="btn">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title : ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+
+            if(this.title) {
+                const newTodo = {
+                    title: this.title,
+                    completed: false
+                }
+
+                this.$emit('add-todo', newTodo);
+
+                this.title = '';
+            } 
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 7;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 3;
+    }
+
+    .btn {
+        background-color: #008CBA; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background-color: #134d61;
+    }
+
+    .btn:active {
+        background-color: #134d61;
+    }
+</style>
